724a7314fb6d986ab9995a25f55abb202c6de4b0
ww8007 <wshmin1234@gmail.com>
2022-12-17 17:23:58 +0900

    build: project 초기 셋팅 with turbopack

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/styles/dist.css';
+import React from 'react';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head>
+        <title>Next.js Turbopack App Directory Playground</title>
+      </head>
+      <body className="overflow-y-scroll bg-zinc-900"></body>
+    </html>
+  );
+}
